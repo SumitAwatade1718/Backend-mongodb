6922542e2068f8e2d3d5e1306b4ccaed36e52d45
SumitAwatade1718 <159120851+SumitAwatade1718@users.noreply.github.com>
2025-04-10 19:13:34 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose')
+
+
+
+const prodcuctSchema=mongoose.Schema({
+
+    productName:{type:String,required:true,unique:true},
+
+    productPrice:{type:Number,required:true},
+
+    productUnit:{type:String,required:true},
+
+    productDescription:{type:String,required:true},
+
+})
+
+
+
+module.exports=mongoose.model('product',prodcuctSchema)
